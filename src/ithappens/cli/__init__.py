986fb3a983460a5151d40ba23dd78f19da56ca3c
siemdejong <28396796+siemdejong@users.noreply.github.com>
2024-10-04 18:53:10 +0200

    feat: use input files and make ui easier
    Fixes #66
    
    BREAKING CHANGE: it is no longer possible to use an input directory.
    Invididual input files have to be selected.

--- src/ithappens/cli/__init__.py
+++ src/ithappens/cli/__init__.py
@@ -10,7 +10,10 @@ def cli():
 
 
 @cli.command(help="Create cards.")
-@click.argument("input_dir")
+@click.argument(
+    "input_file", type=click.Path(exists=True, dir_okay=False, readable=True)
+)
+@click.argument("output_dir", type=click.Path(file_okay=False))
 @click.option(
     "-n",
     "--name",
@@ -25,6 +28,14 @@ def cli():
     type=click.Choice(["front", "back", "both"], case_sensitive=False),
     default="both",
     help="Side(s) to generate.",
+)
+@click.option(
+    "-e",
+    "--expansion_logo_path",
+    "expansion_logo_path",
+    type=click.Path(exists=True, dir_okay=False, readable=True),
+    default=None,
+    help="Expansion logo path.",
 )
 @click.option(
     "-f",
